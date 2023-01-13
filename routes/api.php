4a4fd01c773a5aaadafea24f384dccbbc62d4927
Eugene Musienko <zdravstvuy1714@gmail.com>
2023-01-14 00:25:38 +0600

    wip

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,3 @@
 declare(strict_types=1);
 
 /** @var Illuminate\Routing\Router $router */
-
-use App\Http\Controllers\ShowApplicationInformation;
-
-$router->get('/', ShowApplicationInformation::class);
