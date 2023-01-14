e447ba8ec48de47378246a112969a7c537712a5d
Eugene Musienko <zdravstvuy1714@gmail.com>
2023-01-15 03:39:58 +0600

    feat (logging): added channel for deprications

--- config/logging.php
+++ config/logging.php
@@ -72,6 +72,11 @@ return [
             'days' => 14,
         ],
 
+        "deprecations" => [
+            "driver" => "single",
+            "path" => storage_path('logs/deprecation-warnings.log'),
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
