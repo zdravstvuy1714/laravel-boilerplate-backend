bb53dda749f489703eb25a75be15602e4ab3ad1f
Eugene Musienko <zdravstvuy1714@gmail.com>
2023-01-15 03:47:14 +0600

    wip

--- config/logging.php
+++ config/logging.php
@@ -69,12 +69,12 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 7,
         ],
 
-        "deprecations" => [
-            "driver" => "single",
-            "path" => storage_path('logs/deprecation-warnings.log'),
+        'deprecations' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/deprecation-warnings.log'),
         ],
 
         'slack' => [
