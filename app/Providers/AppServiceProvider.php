394198bc416616af0c911a6de4db8b9068e985c2
Eugene Musienko <zdravstvuy1714@gmail.com>
2023-01-15 03:19:00 +0600

    feat (app): completed configuration of application service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use UnhandledMatchError;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -19,11 +20,14 @@ final class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        /** @var string $environment */
+        $environment = $this->app->environment();
+
         // Configuring default password rules.
-        Password::defaults(function () {
+        Password::defaults(static function () use ($environment) {
             $rule = Password::min(6);
 
-            return match ($this->app->environment()) {
+            return match ($environment) {
                 EnvironmentEnum::Production->value,
                 EnvironmentEnum::Testing->value => $rule
                     ->letters()
@@ -32,6 +36,7 @@ final class AppServiceProvider extends ServiceProvider
                     ->symbols()
                     ->uncompromised(),
                 EnvironmentEnum::Local->value => $rule,
+                default => throw new UnhandledMatchError($environment),
             };
         });
 
