b8f4a84a92a26d2c8e6d7006efd61abe24262a88
Eugene Musienko <zdravstvuy1714@gmail.com>
2023-01-15 03:35:58 +0600

    wip

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,16 @@
 declare(strict_types=1);
 
 /** @var Illuminate\Routing\Router $router */
+
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
+
+$router->get('/', function () {
+    $user = \Modules\User\Entities\User::query()->first();
+
+    Mail::raw('Hi, welcome user!', static function (Message $message) use ($user) {
+        $message
+            ->to($user->email)
+            ->subject('Тема');
+    });
+});
