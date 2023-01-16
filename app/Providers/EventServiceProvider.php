3434e92156e5b75ef4c67e2868b434e026cf3b0c
Eugene Musienko <zdravstvuy1714@gmail.com>
2023-01-16 23:15:23 +0600

    wip

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -11,9 +11,4 @@ final class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
     }
-
-    public function shouldDiscoverEvents(): bool
-    {
-        return false;
-    }
 }
